2c742d99495659b6ab7c9ea9c8726eb8232ee408
Inigo Hidalgo <inigohrey@gmail.com>
2020-08-30 15:46:59 +0200

    Fixing to make work on laptop and desktop

--- copy-and-open-template.sh
+++ copy-and-open-template.sh
@@ -1,16 +1,20 @@
 template=~/vscode-notetaking/vscode-latex-notetaking/template.svg
 
 default='template'
+blank='blank'
 old_filename=${1:-default}
-mkdir figures
+working_directory=${2:-blank}
+fig_dir="${working_directory}/figures"
+mkdir "$fig_dir"
+echo $fig_dir
 
 filename=$(echo $old_filename | tr ' ' '_')
-filepath=figures/$filename
+filepath="$working_directory/figures/$filename"
 
-cp -n $template $filepath.svg
+cp -n $template "$filepath.svg"
 
-python3 ~/vscode-notetaking/vscode-latex-notetaking/inkscape-shortcut-manager/main.py & inkscape $filepath.svg
+python3 ~/vscode-notetaking/vscode-latex-notetaking/inkscape-shortcut-manager/main.py & inkscape "$filepath.svg"
 
-inkscape -D $filepath.svg -A $filepath.pdf --export-latex
+inkscape -D "$filepath".svg -A "$filepath".pdf --export-latex
 
 echo $filename
